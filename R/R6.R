b8a6fc6f26aad14f751ccaa35d125fecfb56d4b5
John Coene <jcoenep@gmail.com>
2019-08-21 08:31:41 +0200

    sentiment

--- R/R6.R
+++ R/R6.R
@@ -31,8 +31,33 @@ Textblob <- R6::R6Class(
     sentiment = function(){
       map_method(private$.tb, "sentiment", private$to_r)
     },
+    sentiment_polarity = function(){
+      map(private$.tb, function(x, to_r){
+        results <- x$sentiment$polarity
+        if(to_r)
+          results <- py_to_r(results)
+        return(results)
+      }, to_r = private$to_r)
+    },
+    sentiment_subjectivity = function(){
+      map(private$.tb, function(x, to_r){
+        results <- x$sentiment$subjectivity
+        if(to_r)
+          results <- py_to_r(results)
+        return(results)
+      }, to_r = private$to_r)
+    },
     noun_phrases = function(){
       map_method(private$.tb, "noun_phrases", private$to_r)
+    },
+    translate = function(lang){
+      assert_that(!missing(lang))
+      map(private$.tb, function(x, l, to_r){
+        translation <- x$translate(to = l)
+        if(to_r)
+          translation <- py_to_r(translation)
+        return(translation)
+      }, l = lang, to_r = private$to_r)
     }
   ),
   private = list(
